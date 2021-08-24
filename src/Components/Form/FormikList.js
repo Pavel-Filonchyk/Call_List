228d4f92d07e23e2848d1dcd5fc0fd63262f46f4
Pavel Filonchyk <p_filonchyk@mail.ru>
2021-08-25 00:56:51 +0300

    added formReset

--- src/Components/Form/FormikList.js
+++ src/Components/Form/FormikList.js
@@ -3,9 +3,12 @@ import {Formik, Form, Field} from 'formik';
 import './Form.css'
 
 export default function FormikList({offModal, onSubmit}) {
-  const submit = (values, {setSubmitting}) => {
-    onSubmit(values)  
-  }                   
+  const submit = (values, {setSubmitting, resetForm}) => {
+    onSubmit(values)
+    setSubmitting(false)
+    resetForm()
+  } 
+                    
     return (
       <div>
         <Formik
@@ -13,17 +16,17 @@ export default function FormikList({offModal, onSubmit}) {
         onSubmit={submit}
         >
         {({ isSubmitting }) => (
-         <Form>
+          <Form>
             <div data-close className="modal__close"
               onClick={offModal}
             >&times;</div>
-           <Field component="input" className="modal__input"  name="first_name" type="text" required placeholder="first name"/>
+            <Field component="input" className="modal__input"  name="first_name" type="text" required placeholder="first name"/>
               <Field component="input" className="modal__input"  name="last_name" type="text" required placeholder="last name"/>
               <Field component="input" className="modal__input"  name="number" type="text" required placeholder="Phone number"/>
               <Field component="input" className="modal__input"  name="message" type="text" required placeholder="Message"/>
-           <button type="submit" disabled={isSubmitting}>
-             Sender
-           </button>
+            <button type="submit" disabled={isSubmitting}>
+              Sender
+            </button>
           </Form>
         )}
       </Formik>
